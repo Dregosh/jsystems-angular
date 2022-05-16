ba415ff80ebe0b6758973e9f66cb5209d6befc70
Mateusz Kulesza <mateusz@altkom.pl>
2022-05-16 14:15:21 +0200

    Nested Routing - route children

--- src/app/playlists/playlists-routing.module.ts
+++ src/app/playlists/playlists-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaylistsViewComponent } from './containers/playlists-view/playlists-view.component';
 import { PlaylistsComponent } from './playlists.component';
 
 const routes: Routes = [{
   path: '',
-  component: PlaylistsComponent
+  component: PlaylistsComponent,
+  children: [
+    {
+      path: '',
+      component: PlaylistsViewComponent
+    }
+  ]
 }];
 
 @NgModule({
