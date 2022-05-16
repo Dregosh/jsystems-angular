49a34765dc31648815ed4a21731d8e446b146faa
Mateusz Kulesza <mateusz@altkom.pl>
2022-05-16 15:16:25 +0200

    [ngModel]  (ngModelChange)  $event -> value!

--- src/app/playlists/playlists.module.ts
+++ src/app/playlists/playlists.module.ts
@@ -19,7 +19,6 @@ import { SharedModule } from '../shared/shared.module';
     PlaylistEditorComponent
   ],
   imports: [
-    CommonModule,
     SharedModule,
     PlaylistsRoutingModule
   ]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   exports: [
+    CommonModule,
     ClockComponent,
     FormsModule
   ]
