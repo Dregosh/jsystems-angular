92835cbd01ec76641e3ef0ee3fc46d746eaafcc5
Mateusz Kulesza <mateusz@altkom.pl>
2022-05-17 14:06:22 +0200

    Excercise: Use Mock Album Data to render Mock Results View

--- src/app/music/containers/album-search-view/album-search-view.component.ts
+++ src/app/music/containers/album-search-view/album-search-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { mockAlbums } from 'src/app/core/mocks/mockAlbums';
+import { Album } from 'src/app/core/model/Search';
 
 @Component({
   selector: 'app-album-search-view',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-search-view.component.scss']
 })
 export class AlbumSearchViewComponent implements OnInit {
+
+  results: Album[] = mockAlbums
+
+  search(query = 'batman') {
+    console.log(query);
+  }
 
   constructor() { }
 
