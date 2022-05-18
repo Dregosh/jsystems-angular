0219baf811bdba9091503c756b3eaee47a4be864
Mateusz Kulesza <mateusz@altkom.pl>
2022-05-18 14:48:41 +0200

    Navigate, relativeTo, queryParams + Reactive Routing > querParamMap observable

--- src/app/music/containers/album-search-view/album-search-view.component.ts
+++ src/app/music/containers/album-search-view/album-search-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
 import { mockAlbums } from 'src/app/core/mocks/mockAlbums';
 import { Album } from 'src/app/core/model/Search';
 import { MusicApiService } from 'src/app/core/services/music-api/music-api.service';
@@ -22,24 +23,33 @@ export class AlbumSearchViewComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const q = this.route.snapshot.queryParamMap.get('q')
-    if (q) {
-      this.query = q
+    // const q = this.route.snapshot.queryParamMap.get('q')
+
+    this.route.queryParamMap.pipe(
+      map(qm => qm.get('q')),
+      filter((q: any): q is string => typeof q === 'string'),
+      tap(q => { this.query = q }),
+    ).subscribe(q => {
       this.searchAlbums(q)
-    }
+    })
   }
 
   searchAlbums(query = 'batman') {
+
+    this.router.navigate([
+      /* '/music', 'search' */
+      /* '..' */
+      /* './sibling' */
+    ], {
+      relativeTo: this.route,
+      queryParams: { q: query }
+    })
 
     this.service
       .fetchAlbumSearchResults(query)
       .subscribe({
         next: res => this.results = res,
         error: error => this.message = error.message,
-        // complete: () => console.log('complete'),
       })
-
   }
-
-
 }
