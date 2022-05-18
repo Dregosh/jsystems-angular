8e296ee1df6458c4a0850536bcda5e0a8e81c89a
Mateusz Kulesza <mateusz@altkom.pl>
2022-05-18 10:47:14 +0200

    Union types,  Possibly undefined,  Handling undefined

--- src/app/core/model/Playlist.ts
+++ src/app/core/model/Playlist.ts
@@ -1,6 +1,31 @@
+
+interface Track {
+  id: string;
+  name: string;
+  type: "track";
+}
+
+
 export interface Playlist {
   id: string;
   name: string;
   public: boolean;
   description: string;
+  tracks?: Track[]
 }
+
+// const p = {} as any
+
+const p = {} as Playlist
+
+if (p.tracks) {
+  const len = p.tracks.length
+}
+const len1 = p.tracks?.length // number | undefined
+const len2 = (p.tracks as Track[]).length // number ?
+const len3 = p.tracks!.length // number ?
+
+const len4 = p.tracks ? p.tracks.length : 0
+const len5 = p.tracks?.length ?? 0
+const len6 = p.tracks?.length || 0
+
